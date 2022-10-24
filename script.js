77d50d7fcdb33b93e38e5d2bd5956a928434e4f3
Michael Huang <michaelh@casual.place>
2022-10-23 18:53:14 -0700

    Add feature to change board size
    There's now a button that prompts the user to input a number.
    The board will then reset to use that number as its dimensions in terms
    of how many 'pixel' divs are inside.
    
    We noticed that every time the board resets, we have to re-query each
    'pixel' div to give each of them the ability to change color. So we
    separated that action into a dedicated function.

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 const drawingBoard = document.querySelector('.drawing-board');
+const sizeButton = document.querySelector('.size');
+const resetButton = document.querySelector('.reset');
+
+sizeButton.addEventListener('click', selectSize);
+resetButton.addEventListener('click', resetBoard);
 
 function create(dimension) {
     for (let i = 0; i < dimension; i++) {
@@ -17,19 +22,30 @@ function colorIn(e) {
     this.style.backgroundColor = "black";
 }
 
+function refreshPixelSelector() {
+    const pixel = document.querySelectorAll('.pixel');
+    pixel.forEach(p => p.addEventListener('mouseenter', colorIn));
+}
+
 function resetBoard(e) {
     const pixel = document.querySelectorAll('.pixel');
     pixel.forEach(p => p.style.backgroundColor = "white");
 }
 
+function selectSize() {
+    let size = parseInt(prompt('Change size (between 1-100', '16'));
+    while (isNaN(size) || (size > 100) || (size < 1)) {
+        size = parseInt(prompt('Only enter an integer from 1-100', '16'));
+    }
+    const rows = document.querySelectorAll('.row');
+    rows.forEach(r => r.remove());
+    create(size);
+    refreshPixelSelector();
+}
+
 function run() {
     create(16);
-
-    const pixel = document.querySelectorAll('.pixel');
-    pixel.forEach(p => p.addEventListener('mouseenter', colorIn));
-
-    const resetButton = document.querySelector('.reset');
-    resetButton.addEventListener('click', resetBoard);
+    refreshPixelSelector();
 }
 
 run();
